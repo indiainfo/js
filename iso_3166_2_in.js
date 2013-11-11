de4edb6bc391836ed1b77a3a140c843ce804441b
indiainfo <pickpol@gmail.com>
2013-11-11 22:54:57 +0530

    Create iso_3166_2_in.js

---
+++ iso_3166_2_in.js
@@ -0,0 +1,107 @@
+var iso_3166_2_in = {
+    "IN-AP": {
+        "name": "Andhra Pradesh"
+    },
+    "IN-AR": {
+        "name": "Arunachal Pradesh"
+    },
+    "IN-AS": {
+        "name": "Assam"
+    },
+    "IN-BR": {
+        "name": "Bihar"
+    },
+    "IN-CT": {
+        "name": "Chhattisgarh"
+    },
+    "IN-GA": {
+        "name": "Goa"
+    },
+    "IN-GJ": {
+        "name": "Gujarat"
+    },
+    "IN-HR": {
+        "name": "Haryana"
+    },
+    "IN-HP": {
+        "name": "Himachal Pradesh"
+    },
+    "IN-JK": {
+        "name": "Jammu and Kashmir"
+    },
+    "IN-JH": {
+        "name": "Jharkhand"
+    },
+    "IN-KA": {
+        "name": "Karnataka"
+    },
+    "IN-KL": {
+        "name": "Kerala"
+    },
+    "IN-MP": {
+        "name": "Madhya Pradesh"
+    },
+    "IN-MH": {
+        "name": "Maharashtra"
+    },
+    "IN-MN": {
+        "name": "Manipur"
+    },
+    "IN-ML": {
+        "name": "Meghalaya"
+    },
+    "IN-MZ": {
+        "name": "Mizoram"
+    },
+    "IN-NL": {
+        "name": "Nagaland"
+    },
+    "IN-OR": {
+        "name": "Orissa"
+    },
+    "IN-PB": {
+        "name": "Punjab"
+    },
+    "IN-RJ": {
+        "name": "Rajasthan"
+    },
+    "IN-SK": {
+        "name": "Sikkim"
+    },
+    "IN-TN": {
+        "name": "Tamil Nadu"
+    },
+    "IN-TR": {
+        "name": "Tripura"
+    },
+    "IN-UP": {
+        "name": "Uttar Pradesh"
+    },
+    "IN-UT": {
+        "name": "Uttarakhand"
+    },
+    "IN-WB": {
+        "name": "West Bengal"
+    },
+    "IN-AN": {
+        "name": "Andaman and Nicobar Islands"
+    },
+    "IN-CH": {
+        "name": "Chandigarh"
+    },
+    "IN-DN": {
+        "name": "Dadar and Nagar Haveli"
+    },
+    "IN-DD": {
+        "name": "Daman and Diu"
+    },
+    "IN-DL": {
+        "name": "Delhi"
+    },
+    "IN-LD": {
+        "name": "Lakshadweep"
+    },
+    "IN-PY": {
+        "name": "Pondicherry"
+    }
+};
